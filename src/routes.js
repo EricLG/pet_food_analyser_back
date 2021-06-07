f373d0c6a195071022997e0df9362eb4c5f644f0
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2021-06-07 21:33:18 +0200

    Delete Hello world

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,5 @@
-import { TEST } from './api'
 import { petfoodRoutes } from './api/petfood'
 
 export const initRoutes = (app => {
-    app.get('/', (req, res) => {
-        console.log(TEST)
-        res.send({
-            message: 'hello world8'
-        })
-    })
     petfoodRoutes(app)
-
 })
